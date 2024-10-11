2d0a9dcf9e955749e44ffeff7f4a791605d32623
Xudoynazar1112 <141805748+Xudoynazar1112@users.noreply.github.com>
2024-10-11 23:25:46 +0500

    optimazed endpoints

--- api/urls.py
+++ api/urls.py
@@ -3,8 +3,8 @@ from . import views
 
 urlpatterns = [
     path("", views.UserListView.as_view(), name="user"),
-    path("create/", views.UserCreateView.as_view(), name="user"),
-    path("create/<pk>/", views.UserDetailView.as_view(), name="detail"),
+    path("user/", views.UserCreateView.as_view(), name="user"),
+    path("user/<pk>/", views.UserDetailView.as_view(), name="detail"),
     path('generate-username/', views.generate_username, name='generate_username'),
     path('generate-password/', views.generate_password, name='generate_password'),
 ]
